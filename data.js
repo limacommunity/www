126c333cbe828a9bd3d69a257368edd7d9f1e150
limacctech <tech@limacc.com>
2025-12-16 21:06:57 -0500

    Initial Commit

---
+++ data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-holy-forever-most-hands-raised-360",
+      "name": "Holy-Forever-Most-Hands-Raised-360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -0.15090835726707041,
+        "pitch": 0.0006780304257407721,
+        "fov": 1.4996545601207187
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
